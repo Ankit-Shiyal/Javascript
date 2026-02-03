77b1a668a5cd8feee1891b3dff05cf3b1434a299
Ankit-Shiyal <ankitshiyal2005@gmail.com>
2026-02-03 14:35:14 +0530

    Add files via upload

---
+++ ch-5_js-TypeConvertion/index.js
@@ -0,0 +1,161 @@
+// 1
+console.log("3" - 2);
+
+// 2
+console.log("4" + 2);
+
+//3
+console.log("10" * 2);
+
+//4
+console.log("8" / 2);
+
+//5
+console.log(false + 1);
+
+//6
+console.log(true + 1);
+
+//7
+console.log("1" + false);
+
+//8
+console.log("1" + true);
+
+//9
+console.log(undefined + 1);
+
+//10
+console.log(null + 2);
+
+//11
+console.log(+"15" + 10);
+
+//12
+console.log(true - 1);
+
+//13
+console.log(false - "1");
+
+//14
+console.log(Boolean(""));
+
+//15
+console.log(Boolean("1"));
+
+//16
+console.log(Boolean("false"));
+
+//17
+console.log(Boolean("true"));
+
+//18
+console.log(Boolean(undefined));
+
+//19
+console.log("Ankit" - 5);
+
+//20
+console.log("Ankit" + 5);
+
+//21
+let A = 10;
+console.log(String(A) + 5);
+
+//22
+let B = 10;
+console.log(Number(B) + 5);
+
+//23
+let C = 10;
+console.log(String(C) - 5);
+
+//24
+let D = 10;
+console.log(Number(D) - 5);
+
+//25
+console.log(Number(true) + Number(false));
+
+//26
+console.log(Number(true) - Number(false));
+
+//27
+console.log(String(true) + String(false));
+
+//28
+console.log(parseInt("23px"));
+
+//29
+console.log(parseInt("20.5km"));
+
+//30
+console.log(parseFloat("23.5kg"));
+
+//31
+console.log(Number(" ")); 
+
+//32
+console.log(Number("10a"));
+
+//33
+console.log(parseFloat("10.5a"));
+
+//34
+console.log(Number("10"));   
+
+//35
+console.log(String(25));          
+
+//36
+console.log(Number(true));        
+
+//37 
+console.log(Number(false));       
+
+//38
+console.log(Boolean(1));          
+
+//39
+console.log(Boolean(0));
+
+//40
+console.log(Boolean(-5));    
+
+//41
+console.log(Number(null)); 
+
+//41
+console.log(Number(undefined)); 
+
+//42
+console.log(String(20));      
+
+//43
+console.log(String(undefined));   
+
+//44
+console.log(Boolean(null)); 
+
+//45
+console.log(Boolean(undefined));
+
+//46
+console.log(parseInt("20.5kg") - 3);
+
+//47
+console.log(parseFloat("20.5kg") - 3);
+
+//48
+console.log(Number("123"));       
+
+//49
+console.log(String(123));         
+
+//50
+console.log(Boolean(123));  
+
+
+
+
+
