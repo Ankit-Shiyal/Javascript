32cb8c073f60b94958a9959e3d91014877975fe5
Ankit-Shiyal <ankitshiyal2005@gmail.com>
2026-02-03 17:50:19 +0530

    Add files via upload

---
+++ ch-6_js-ControlFlow/nested.js
@@ -0,0 +1,35 @@
+
+
+//1
+
+let Age = 19;
+
+let voterId = true;
+
+if (Age >= 18) {
+  if (voterId) {
+    console.log("you can give vote");
+  } else {
+    console.log("You can apply voterID");
+  }
+} 
+
+else {
+  console.log("You cannot give vote");
+}
+
+//2
+
+let drivingLicense= false;
+
+if (Age >= 18) {
+  if (drivingLicense) {
+    console.log("you can Drive car");
+  } else {
+    console.log("You can apply for driving License");
+  }
+} 
+
+else {
+  console.log("You can't  Drive car");
+}
