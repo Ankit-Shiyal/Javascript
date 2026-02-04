9d75c9175d8e694bf94dc489a447572fa5205673
Ankit-Shiyal <ankitshiyal2005@gmail.com>
2026-02-04 10:21:05 +0530

    Add files via upload

---
+++ ch-6_js-ControlFlow/tarnary.js
@@ -0,0 +1,15 @@
+
+
+//1
+let marks = 85;
+
+let result = (marks >= 33) ? "you are Pass" : "Fail";
+console.log(result);
+
+//2
+
+
+let num = 7;
+
+let check = (num % 2 === 0) ? "number is Even" : "number is Odd";
+console.log(check);
