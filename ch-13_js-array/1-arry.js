1107fcaacc8ea06d3f2f8d0350b9c14f1d8e1649
Ankit-Shiyal <ankitshiyal2005@gmail.com>
2026-02-25 13:24:20 +0530

    Add files via upload

---
+++ ch-13_js-array/1-arry.js
@@ -0,0 +1,15 @@
+
+//1 
+let fruits =["apple", "orange", "banana", "strawberry", "guava"]
+
+console.log(typeof fruits)
+
+console.log("fruits name :", fruits)
+
+//2
+
+let number = ["11", "12" ,"13", "14", "15"]
+console.log("number :", number)
+
+
+
