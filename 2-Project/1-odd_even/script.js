d247487f4b729f7c567d757ffe6b4b8bbddfec74
Ankit-Shiyal <ankitshiyal2005@gmail.com>
2026-03-06 09:44:26 +0530

    Add files via upload

---
+++ 2-Project/1-odd_even/script.js
@@ -0,0 +1,24 @@
+document.getElementById("form").addEventListener("submit", (e) => {
+
+  e.preventDefault();
+
+  let num = Number(document.getElementById("num").value);
+
+  let even = 0;
+  let odd = 0;
+
+  for (let i = 1; i <= num; i++) {
+
+    if (i % 2 === 0) {
+      even++;
+    } else {
+      odd++;
+    }
+
+  }
+
+  document.getElementById("result").innerHTML =
+    "Even Numbers: " + even + "<br>" +
+    "Odd Numbers: " + odd;
+
+});
