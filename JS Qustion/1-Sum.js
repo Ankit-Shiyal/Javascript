dc3b4e54598ac35ab931879622d45bfab9793bb2
Ankit-Shiyal <ankitshiyal2005@gmail.com>
2026-03-20 15:06:03 +0530

    Add files via upload

---
+++ JS Qustion/1-Sum.js
@@ -0,0 +1,24 @@
+//function
+
+function sum(a,b){
+    let sum = a + b;
+
+    console.log("sum :-",sum)
+
+}
+
+sum(12,8)
+
+// loop
+
+const number=[10,30,40,70];
+let result=0;
+
+for(let i=0; i<number.length; i++){
+    
+
+    result += number[i];
+}
+
+console.log("sum :-",result)
+
