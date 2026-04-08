82fe282188ddbbb8265c09ebbf27a9fca1f992af
Ankit-Shiyal <ankitshiyal2005@gmail.com>
2026-04-08 12:16:31 +0530

    Add files via upload

---
+++ js array/1-array.js
@@ -0,0 +1,15 @@
+
+
+// An array in JavaScript is a data structure used to store multiple values in a single variable.
+
+
+let fruits = ["apple", "banana", "orange", "guava", ]
+
+console.log(typeof fruits)
+
+console.log("fruit", fruits)
+
+// array constructor
+
+let fruitsArray = new Array ("apple", "banana", "guava")
+console.log("new array", fruitsArray)
---
+++ js array/2-array.js
@@ -0,0 +1,24 @@
+
+
+let fruits = ["Apple", "Banana", "Cherry", "Orang", "Mango"]
+
+console.log("fruits Array :",fruits)
+
+// first fruits 
+
+console.log("first :", fruits[0])
+
+// last fruits
+
+console.log("last :", fruits[fruits.length-1])
+
+console.log("any one :", fruits[3])
+
+
+console.log("change second element")
+
+fruits[1] = "Strawberry"
+console.log("any one :", fruits[1])
+
+
+console.log("change second element in array:",fruits)
