efe455a64315f89236952f7d5f742c7572f66ed3
Ankit-Shiyal <ankitshiyal2005@gmail.com>
2026-04-16 14:18:17 +0530

    Add files via upload

---
+++ ch-20_js-typeScript/exampal.js
@@ -0,0 +1,8 @@
+"use strict";
+let userName = "Ankit";
+let num1 = 22;
+let num2 = 33;
+function addition(x, y) {
+    return x + y;
+}
+console.log("addition :", addition(num1, num2));
---
+++ ch-20_js-typeScript/exampal.ts
@@ -0,0 +1,14 @@
+
+
+let userName : string ="Ankit"
+
+let num1 : number = 22
+let num2 : number =33
+
+function addition(x:number , y:number):number{
+
+    return x+y
+
+}
+
+console.log ("addition :" ,addition(num1,num2))
