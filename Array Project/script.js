5da373630d5c2b921a93f97123b4f75907e3b400
Ankit-Shiyal <ankitshiyal2005@gmail.com>
2026-04-20 15:41:54 +0530

    Add files via upload

--- Array Project/script.js
+++ Array Project/script.js
@@ -1,19 +1,19 @@
-
 let output = document.getElementById("output");
-let start = document.getElementById("start")
 let pushBtn = document.getElementById("pushBtn");
 let popBtn = document.getElementById("popBtn");
 let shiftBtn = document.getElementById("shiftBtn");
 let unshiftBtn = document.getElementById("unshiftBtn");
-let itemArray = document.getElementById("itemArray")
-let showArray = document.getElementById("showArray")
+let itemArray = document.getElementById("itemArray");
+let showArray = document.getElementById("showArray");
 
 let arr = [];
+
 
 function startNum() {
     let numInput = document.getElementById("arrayNum").value;
 
     itemArray.innerHTML = "";
+    arr = []; 
 
     for (let i = 1; i <= numInput; i++) {
         itemArray.innerHTML += `
@@ -22,3 +22,67 @@ function startNum() {
     }
 }
 
+
+function loadArray() {
+    let count = document.getElementById("arrayNum").value;
+
+    arr = [];
+
+    for (let i = 1; i <= count; i++) {
+        let val = document.getElementById("el" + i).value;
+        if (val !== "") {
+            arr.push(val);
+        }
+    }
+
+    updateUI();
+}
+
+
+function updateUI() {
+    showArray.innerHTML = "Array: [ " + arr.join(", ") + " ]";
+    output.innerHTML = "Output: [ " + arr.join(", ") + " ]";
+}
+
+
+showArray.onclick = loadArray;
+
+
+pushBtn.onclick = function () {
+    if (arr.length === 0) loadArray();
+
+    let newItem = prompt("Enter new item");
+    if (newItem !== null) {
+        arr.push(newItem);
+    }
+
+    updateUI();
+};
+
+
+popBtn.onclick = function () {
+    if (arr.length === 0) loadArray();
+
+    arr.pop();
+    updateUI();
+};
+
+
+shiftBtn.onclick = function () {
+    if (arr.length === 0) loadArray();
+
+    arr.shift();
+    updateUI();
+};
+
+
+unshiftBtn.onclick = function () {
+    if (arr.length === 0) loadArray();
+
+    let newItem = prompt("Enter new item");
+    if (newItem !== null) {
+        arr.unshift(newItem);
+    }
+
+    updateUI();
+};
