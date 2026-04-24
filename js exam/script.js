83ea1282c3a1a8ddef009758e35fdd660bac958d
Ankit-Shiyal <ankitshiyal2005@gmail.com>
2026-04-24 15:35:40 +0530

    Add files via upload

--- js exam/script.js
+++ js exam/script.js
@@ -1,9 +1,11 @@
-let task = []
+
+        
+let tasksAdd = JSON.parse(localStorage.getItem("tasksAdd")) || [];
 let index = -1
 
-document.getElementById("form").addEventListener("submit", (e)=> {
+document.getElementById("form").addEventListener("submit", (e) => {
     e.preventDefault()
-    
+
     addData()
 })
 
@@ -11,54 +13,67 @@ function addData() {
 
     let taskInput = document.getElementById("taskInput").value
     let disInput = document.getElementById("disInput").value
+    let DateInput= document.getElementById("DateInput").value
 
-   
+
     let obj = {
         task: taskInput,
-        desc: disInput
+        desc: disInput,
+        date: DateInput
     }
 
     if (index === -1) {
-        task.push(obj)
+        tasksAdd.push(obj)
     } else {
-        task[index] = obj
+        tasksAdd[index] = obj
         index = -1
     }
 
+    saveData()
+
     document.getElementById("taskInput").value = ""
     document.getElementById("disInput").value = ""
-
+    document.getElementById("DateInput").value =""
     display()
 }
-
 function display() {
 
     let result = document.getElementById("result")
     result.innerHTML = ""
 
-    task.forEach((item, id) => {
+    tasksAdd.forEach((item, id) => {
         result.innerHTML += `
-            <div>
-                <b>${item.task}</b> - ${item.desc}
-                <button onclick="editData(${id})">Edit</button>
-                <button onclick="removeData(${id})">Remove</button>
-
-                
-            </div>
+            <tr>
+                <td><h5>${id + 1}<h5></td>
+                <td>${item.task}</td>
+                <td>${item.desc}</td>
+                <td>${item.date}</td>
+                <td><button class="btn btn-outline-info" onclick="editData(${id})">Edit</button></td>
+                <td><button class="btn btn-outline-danger" onclick="removeData(${id})">Remove</button></td>
+            </tr>
         `
-    
     })
 }
 
 function removeData(id) {
-    task.splice(id, 1)
+    tasksAdd.splice(id, 1)
+    saveData()
     display()
 }
 
 function editData(id) {
-     index = id
-    document.getElementById("taskInput").value = task[id].task
-    document.getElementById("disInput").value = task[id].desc
+    index = id
+    document.getElementById("taskInput").value = tasksAdd[id].task
+    document.getElementById("disInput").value = tasksAdd[id].desc
+    document.getElementById("DateInput").value= tasksAdd[id].date
 
-   
 }
+
+
+function saveData() {
+    localStorage.setItem("tasksAdd", JSON.stringify(tasksAdd));
+}
+
+display();
+
+    
